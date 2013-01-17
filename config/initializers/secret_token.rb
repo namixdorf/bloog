0a0a7860c2497ee2d002b8a3544d8f22d6abe4c9
Nick Mixdorf <namixdorf@gmail.com>
2013-01-17 08:42:06 -0600

    first commit for objects in rails by Avdi Grimm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloog::Application.config.secret_token = '7af8161ff41c9f980c2e60f96bf7f81d54a20a66be2f28f046eddd796a87f2b07cd609b24a913c59bedcdcce1f96812646cdcb8754bd4c0f10e74809079880df'
